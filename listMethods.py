07cbcd2c0bd75983b837bef5c5b30f97399dd6e1
Cyebukayire <peacecyebukayire@gmail.com>
2021-07-09 12:57:14 +0200

    List Methods

--- listMethods.py
+++ listMethods.py
@@ -1,8 +1,19 @@
-numbers = [2,4,5,9,1,0]
-newNumber = input("Enter New Number: ")
-numbers.append(newNumber)
+numbers = [2,4,5,0,9,1,0]
 print (numbers)
+print("\nAPPEND METHOD")
+newNumber = input("Enter number to Append: ")
+numbers.append(newNumber)
+print(numbers)
 
 # insert at the beginning
-numbers.insert(0, "New")
+print("\nINSERT METHOD")
+newNum = input("Enter number to insert: ")
+index = input("Enter index number: ")
+numbers.insert(index, newNum)
+print(numbers)
+
+# remove item
+print("\nDELETE METHOD")
+delete = input("Enter number to remove: ")
+numbers.remove(delete)
 print(numbers)
