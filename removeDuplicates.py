b20e4edb08d64c30221552072b00428cc4374640
Cyebukayire <peacecyebukayire@gmail.com>
2021-07-09 21:18:10 +0200

    Remove Duplicates from a list

--- removeDuplicates.py
+++ removeDuplicates.py
@@ -1,11 +1,13 @@
 list = [1,2,3,5,1,2,3,4,5,4]
 print(list)
-for item in list:
-    count = list.count(item)
-    print (str(item) +" exists "+ str(count) +" times")
-    if count>1:
-        list.remove(item)
-print(list)
+
+# This method is not efficient
+# for item in list:
+#     count = list.count(item)
+#     # print (str(item) +" exists "+ str(count) +" times")
+#     if count>1:
+#         list.remove(item)
+# print(list)
 
 # new method
 unique = []
@@ -13,3 +15,6 @@ for number in list:
     if number not in unique:
         unique.append(number)
 print(unique)
+
+# muzigahono123@gmail.com
+# muziga10
