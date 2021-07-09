f5235279003fe6cf18053a856df2be2fd6d646d3
Cyebukayire <peacecyebukayire@gmail.com>
2021-07-09 13:15:11 +0200

    List Methods: Check esistance of a number in a list

--- listMethods.py
+++ listMethods.py
@@ -1,27 +1,37 @@
 numbers = [2,4,5,0,9,1,0]
-print (numbers)
-print("\nAPPEND METHOD")
-newNumber = input("Enter number to Append: ")
-numbers.append(newNumber)
-print(numbers)
+# print (numbers)
+# print("\nAPPEND METHOD")
+# newNumber = input("Enter number to Append: ")
+# numbers.append(newNumber)
+# print(numbers)
 
-# insert at the beginning
-print("\nINSERT METHOD")
-newNum = input("Enter number to insert: ")
-index = input("Enter index number: ")
-numbers.insert(index, newNum)
-print(numbers)
+# # insert at the beginning
+# print("\nINSERT METHOD")
+# newNum = input("Enter number to insert: ")
+# index = input("Enter index number: ")
+# numbers.insert(index, newNum)
+# print(numbers)
 
-# remove item
-print("\nDELETE METHOD")
-delete = input("Enter number to remove: ")
-numbers.remove(delete)
-print(numbers)
+# # remove item
+# print("\nDELETE METHOD")
+# delete = input("Enter number to remove: ")
+# numbers.remove(delete)
+# print(numbers)
 
-# clear the list
-# numbers.clear() #not workin
+# # clear the list
+# # numbers.clear() #not workin
 
-# delete at the end
-print("\nPOP METHOD")
-numbers.pop()
-print (numbers)
+# # delete at the end
+# print("\nPOP METHOD")
+# numbers.pop()
+# print (numbers)
+
+# # Check for Location of item in list
+# print("\nINDEX METHOD")
+# numbers.index(9)
+
+# # Check for existance of item in list
+print("\nEXISTANCE OF AN ITEM")
+num = input("Enter any number to check: ")
+print("Item exists: " + str(num in numbers))
+
