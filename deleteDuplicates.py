372dd84eea2f571f0f445b12b3ae3b97fa9ab3b4
Cyebukayire <peacecyebukayire@gmail.com>
2021-07-09 14:12:35 +0200

    Delete Duplicates from a list

--- deleteDuplicates.py
+++ deleteDuplicates.py
@@ -1,4 +1,8 @@
-list = [223,34,2,3,23,0,5,23,2,223]
+list = [1,2,3,5,1,2,3,4,5,4]
+print(list)
 for item in list:
     count = list.count(item)
-    print (str(item) +" exists "+ str(count) +" times")
+    # print (str(item) +" exists "+ str(count) +" times")
+    if count>1:
+        list.remove(item)
+print(list)
