f98e7187d8e5b4f5f57a73287ada3309b777fc37
Cyebukayire <peacecyebukayire@gmail.com>
2021-07-09 12:13:47 +0200

    maximum number from a lit

--- lists.py
+++ lists.py
@@ -1,2 +1,9 @@
 names = ['Mary','John', 'Kelly', 'Musa', 'Cedro']
-print (names[2:0])
+print (names[2:4])
+# maximum number in a list
+numbers = [12,0,5,3,1,9,6]
+max = numbers[0]
+for number in numbers:
+    if max<number:
+        max=number
+print ("The maximum number is : " + str(max))
