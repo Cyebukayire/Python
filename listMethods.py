40de1d7fcdacc87f9d4aee447e0715293ccd7502
Cyebukayire <peacecyebukayire@gmail.com>
2021-07-09 14:04:55 +0200

    List Methods

--- listMethods.py
+++ listMethods.py
@@ -1,39 +1,39 @@
 numbers = [2,4,5,0,9,-67,1,0]
-# print (numbers)
-# print("\nAPPEND METHOD")
-# newNumber = input("Enter number to Append: ")
-# numbers.append(newNumber)
-# print(numbers)
+print (numbers)
+print("\nAPPEND METHOD")
+newNumber = input("Enter number to Append: ")
+numbers.append(newNumber)
+print(numbers)
 
-# # insert at the beginning
-# print("\nINSERT METHOD")
-# newNum = input("Enter number to insert: ")
-# index = input("Enter index number: ")
-# numbers.insert(index, newNum)
-# print(numbers)
+# insert at the beginning
+print("\nINSERT METHOD")
+newNum = input("Enter number to insert: ")
+index = input("Enter index number: ")
+numbers.insert(index, newNum)
+print(numbers)
 
-# # remove item
-# print("\nDELETE METHOD")
-# delete = input("Enter number to remove: ")
-# numbers.remove(delete)
-# print(numbers)
+# remove item
+print("\nDELETE METHOD")
+delete = input("Enter number to remove: ")
+numbers.remove(delete)
+print(numbers)
 
-# # clear the list
-# # numbers.clear() #not workin
+# clear the list
+# numbers.clear() #not workin
 
-# # delete at the end
-# print("\nPOP METHOD")
-# numbers.pop()
-# print (numbers)
+# delete at the end
+print("\nPOP METHOD")
+numbers.pop()
+print (numbers)
 
-# # Check for Location of item in list
-# print("\nINDEX METHOD")
-# numbers.index(9)
+# Check for Location of item in list
+print("\nINDEX METHOD")
+numbers.index(9)
 
-# # Check for existance of item in list
-# print("\nEXISTANCE OF AN ITEM")
-# num = input("Enter any number to check: ")
-# print("Item exists: " + str(num in numbers))
+# Check for existance of item in list
+print("\nEXISTANCE OF AN ITEM")
+num = input("Enter any number to check: ")
+print("Item exists: " + str(num in numbers))
 
 # Count existance of a number
 print("\nCOUNT EXISTANCE OF AN ITEM")
@@ -51,9 +51,9 @@ print("\nREVERSE METHOD")
 numbers.reverse()
 print(numbers)
 
-# Copying a list
-print("\nCOPY METHOD")
-copy = numbers.copy()
-numbers.clear()
-print("Previous list: " + str(numbers))
-print("Coppied List" + str(copy))
+# # Copying a list #NOT WORKING
+# print("\nCOPY METHOD")
+# copy = numbers.copy()
+# numbers.clear()
+# print("Previous list: " + str(numbers))
+# print("Copp List" + str(copy))
