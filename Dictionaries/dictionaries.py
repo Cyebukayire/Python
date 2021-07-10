2d4b41407bca6f9a5b23b0f03b0c1ba6ba857a5b
Cyebukayire <peacecyebukayire@gmail.com>
2021-07-10 17:25:10 +0200

    dictionaries

--- Dictionaries/dictionaries.py
+++ Dictionaries/dictionaries.py
@@ -4,4 +4,8 @@ customers = {
     "is_verified": True
 }
 
-print(customers["name"], customers.get("name"))
+# ways to access value of a key in a dictionary
+print(customers["name"], customers.get("friend", "Ketty Kelly"))
+
+# get method above won't create new key in customers dictionary
+print(customers.get("friend"))
