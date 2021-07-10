a4d920468f7320f5684fe330ac2d138f788f1587
Cyebukayire <peacecyebukayire@gmail.com>
2021-07-10 16:57:26 +0200

    unpacking

---
+++ unpacking.py
@@ -0,0 +1,13 @@
+coordinates = (1,2,3)
+# normal method
+a = coordinates[0]
+b = coordinates[1]
+c = coordinates[2]
+
+# unpacking does the same work
+a, b, c = coordinates#Unpacking
+print (a,b,c)
+
+coordinate = [5,6,7]
+x, y, z = coordinate
+print (x, y, z)
