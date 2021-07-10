b191195164bbf4519d22a4ab0b516e09406e55ac
Cyebukayire <peacecyebukayire@gmail.com>
2021-07-10 17:27:11 +0200

    dictionaries

--- Dictionaries/dictionaries.py
+++ Dictionaries/dictionaries.py
@@ -6,6 +6,8 @@ customers = {
 
 # ways to access value of a key in a dictionary
 print(customers["name"], customers.get("friend", "Ketty Kelly"))
-
 # get method above won't create new key in customers dictionary
-print(customers.get("friend"))
+
+# re-assign the key
+customers["name"] = "Pretty Ketty"
+print(customers.get("name"))
