7ba2bf6f641513835cdc9bc151823de3d809bfdb
Cyebukayire <peacecyebukayire@gmail.com>
2021-07-11 19:24:44 +0200

    Return statement

---
+++ Functions/return.py
@@ -0,0 +1,9 @@
+def square(number):
+    print("printed value is : " + str(number**2))
+    return number * number
+
+print("returned value is "+ str(square(2)))
+
+"""
+by default, all functions in python return none
+"""
