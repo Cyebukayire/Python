98ff7e196c1f17257e61602caab2f18c3f478363
Cyebukayire <peacecyebukayire@gmail.com>
2021-07-11 18:30:12 +0200

    Parameters VS Arguments

---
+++ Functions/functions.py
@@ -0,0 +1,17 @@
+def greet_user(name):
+    print("Hi "+ name +", Welcome to our system")
+
+print ("Start")
+name = "Shallom"
+greet_user(name)
+print("Finish")
+
+""" 
+PARAMETERS VS ARGUMENTS
+
+Arguments are values we pass to functions for be executed while
+Parameters are variables that hold the arguments/values we pass to functions
+
+Arugemnts are defined while calling the funtion while 
+parameters are defined while creating the function
+"""
