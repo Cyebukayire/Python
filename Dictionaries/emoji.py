d28dea508c11ee1437860af0fa3ba4e11205b1fc
Cyebukayire <peacecyebukayire@gmail.com>
2021-07-11 18:01:48 +0200

    emoji

--- Dictionaries/emoji.py
+++ Dictionaries/emoji.py
@@ -1,6 +1,10 @@
 message = raw_input(">")
 words = message.split(" ")
 emoji = {
-    ":)": "smiile",
-    ":(": "sad"
+    ":)": "(smile_face)",
+    ":(": "(sad_face)"
 }
+
+output = ""
+for word in words:
+    output += emoji.get(word, word) +" "
