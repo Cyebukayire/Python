bf41b39a4f046ddc9c158715b4c0b9f84f3062ef
Cyebukayire <peacecyebukayire@gmail.com>
2021-07-11 13:23:26 +0200

    Reporter

--- Dictionaries/translator.py
+++ Dictionaries/translator.py
@@ -1,10 +1,17 @@
 numbers = {
+    0: "Zero",
     1: "One",
     2: "Two",
     3: "Three",
     4: "Four",
-    5: "Five"
+    5: "Five",
+    6: "Six",
+    7: "Seven",
+    8: "Eight",
+    9: "Nine"
 }
-phone = input("Phone: ")
-
+phone = raw_input("Phone: ")
+for item in phone:
+    number = int(item)
+    print(numbers[number])
 # print(numbers[1], numbers[2], numbers[3])
