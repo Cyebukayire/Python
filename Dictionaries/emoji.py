09f62d10a07f127ef0baf210171b3252866c07fd
Cyebukayire <peacecyebukayire@gmail.com>
2021-07-11 18:06:12 +0200

    emojiDictionary

--- Dictionaries/emoji.py
+++ Dictionaries/emoji.py
@@ -1,10 +1,10 @@
+import emojiDictionary
 message = raw_input(">")
 words = message.split(" ")
 emoji = {
     ":)": "(smily_face)",
     ":(": "(sad_face)"
 }
-
 output = ""
 for word in words:
     output += emoji.get(word, word) +" "
