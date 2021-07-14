5377dd24485b8ffca697c0716f0e1c07b25434fd
Cyebukayire <peacecyebukayire@gmail.com>
2021-07-14 15:54:51 +0200

    Ihneritance

--- 6.Inheritance/inheritance.py
+++ 6.Inheritance/inheritance.py
@@ -5,9 +5,16 @@ class Mammal():
     def walk(self):
         print(str(self.name) + " walks")
 
-class Dog(Mammal):
+class Dog(Mammal): #Inheritance
     pass #Python doesn't like emmpty classes, so, this line will fill the gap
+
+
+class Cat(Mammal):
+    pass
+
 
 simba = Dog("Simba")
 simba.walk()
     
+cat = Cat("a cat")
+cat.walk()
