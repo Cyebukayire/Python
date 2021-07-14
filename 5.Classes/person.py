006393d3fb574d1eca2fbe3a8bed1a9158feecf5
Cyebukayire <peacecyebukayire@gmail.com>
2021-07-14 11:45:38 +0200

    Ihneritance

--- 5.Classes/person.py
+++ 5.Classes/person.py
@@ -2,7 +2,7 @@ class Person():
     def __init__(self, name):
         self.name = name
     def talk(self):
-        print("Hi! I am "+ str(self.name))
+        print("Hi! I am "+ self.name)
         return None
 
 
