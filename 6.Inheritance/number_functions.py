b25254b3768092f0145bf2b79224c1158ccbc4c6
Cyebukayire <peacecyebukayire@gmail.com>
2021-07-14 17:54:24 +0200

    Ihneritance

--- 6.Inheritance/number_functions.py
+++ 6.Inheritance/number_functions.py
@@ -1,17 +1,31 @@
-class NumberFunctions():
+class Numbers():
     def __init__(self, list):
         self.list = list
 
     def maximum(self):
-        max=list[0]
+        maxi = list[0]
         for number in list:
-            if max < number:
-                max = number
-        
-        return  max
+            if maxi < number:
+                maxi = number
+        return  maxi
+    
+    def minimum(self):
+        min = list[0]
+        for number in list:
+            if min > number:
+                min = number
+        return min
 
-# list = [2,34,0,3,1,44]
+list = [2,34,0,3,1,44]
+obj = Numbers(list)
+maxi = obj.maximum()
+mini = obj.minimum()
+
+print("The maximum is: " + str(maxi))
+print("The minimum is: " + str(mini))
+
 # maxi = maximum(list)
+# mini = minimum(list)
 # print(maxi)
         
 
