5ec38d5169ba7fe30fbdb36dcbf2abeb2d8a3a69
Cyebukayire <peacecyebukayire@gmail.com>
2021-07-14 16:24:49 +0200

    Ihneritance

--- 6.Inheritance/inheritance.py
+++ 6.Inheritance/inheritance.py
@@ -9,13 +9,6 @@ class Mammal():
 # class Dog(Mammal): #Inheritance
 #     pass #Python doesn't like emmpty classes, so, this line will fill the gap
 
-
-# class Cat(Mammal):
-#     def __init__(self, name):
-#         self.name = name
-#     def be_annoying(self):
-#         print(str(self.name) + " is annoying!")
-
 # simba = Dog("Simba")
 # simba.walk()
     
