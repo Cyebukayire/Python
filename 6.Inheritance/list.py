5a2b78281a7ac52f2cb6b7ddb43550e3f88df080
Cyebukayire <peacecyebukayire@gmail.com>
2021-07-15 06:37:01 +0200

    inheritance

--- 6.Inheritance/list.py
+++ 6.Inheritance/list.py
@@ -1,7 +1,9 @@
 from number_functions import maximum, minimum
 
 list = [3, 100, 4, 23, 0, 22, 97, -1]
+
 max_number = maximum(list)
 min_number = minimum(list)
+
 print ("The maximum number in the list is: " + str(max_number))
-print ("The minimum number in the list is: " + str(max_number))
+print ("The minimum number in the list is: " + str(min_number))
