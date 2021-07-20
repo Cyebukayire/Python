249c63f7e704e4993fd76485a045e93c212de0b4
Cyebukayire <peacecyebukayire@gmail.com>
2021-07-20 09:56:41 +0200

    files

---
+++ 8.Filesystems/app.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+"""
+#Absolute path: path from the root directory
+ex: c:\Program files\Windows
+
+#Relative path: A path starting from the current directory
+"""
+
+path = Path("ecommerce")
+print(path.exists)
