4add8957ddd7d40412b154ec31f870ecad804b3d
Cyebukayire <peacecyebukayire@gmail.com>
2021-07-21 18:37:32 +0200

    app

--- 8.Filesystems/app.py
+++ 8.Filesystems/app.py
@@ -1,15 +1,15 @@
-# from pathlib import Path
-# """
-# #Absolute path: path from the root directory
-# ex: c:\Program files\Windows
+from pathlib import Path
+"""
+#Absolute path: path from the root directory
+ex: c:\Program files\Windows
 
-# #Relative path: A path starting from the current directory
-# """
+#Relative path: A path starting from the current directory
+"""
 
-# path = Path("ecommerce")
-# print(path.exists)
-# if not path.exists:
-#     path.mkdir
-# else:
-#     print("Path Already Exists")
+path = Path("ecommerce")
+print(path.exists)
+if not path.exists:
+    path.mkdir
+else:
+    print("Path Already Exists")
 
