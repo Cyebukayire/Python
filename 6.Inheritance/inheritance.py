55b196612ef70e584e9a2253f8937dbc111c5028
Cyebukayire <peacecyebukayire@gmail.com>
2021-07-22 21:03:18 +0200

    Dog

--- 6.Inheritance/inheritance.py
+++ 6.Inheritance/inheritance.py
@@ -9,27 +9,27 @@ class Mammal(): #Inherited by Dog & Cat in animals module
 """
 DOG OBJECT 
 """
-class Dog(Mammal): #Inheritance
-    pass #Python doesn't like emmpty classes, so, this line will fill the gap
+# class Dog(Mammal): #Inheritance
+#     pass #Python doesn't like emmpty classes, so, this line will fill the gap
 
 
 """
 CAT OBJECT 
 """
-class Cat(Mammal):
-    def __init__(self, name):
-        self.name = name
-    def be_annoying(self):
-        print(str(self.name) + " is annoying!")
+# class Cat(Mammal):
+#     def __init__(self, name):
+#         self.name = name
+#     def be_annoying(self):
+#         print(str(self.name) + " is annoying!")
 
 
 """
 IMPLEMENTATION OF OBJECTS IN THE SAME CLASS AS PARENT CLASS 
 """
-simba = Dog("Simba")
-simba.walk()
+# simba = Dog("Simba")
+# simba.walk()
     
-cat = Cat("my cat")
-cat.walk()
-cat.be_annoying()
+# cat = Cat("my cat")
+# cat.walk()
+# cat.be_annoying()
 
