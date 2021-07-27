d6e2fe2b7373d4c39e01ea7d9da397e60c8ae74c
Cyebukayire <peacecyebukayire@gmail.com>
2021-07-27 10:13:23 +0200

    pathlib

--- 8.Filesystems/search.py
+++ 8.Filesystems/search.py
@@ -2,7 +2,14 @@ from pathlib import Path
 
 path = Path() #Takes current directory
 
-#print all current files in the path directory
+#print all directories & files in the path directory
+for file in path.glob("*"): 
+    print(file)
+    
+#print all files in the path directory
+for file in path.glob("*.*"): 
+    print(file)
 
-for file in path.glob("*"):
+#print all py files in the path directory
+for file in path.glob("*.py"): 
     print(file)
