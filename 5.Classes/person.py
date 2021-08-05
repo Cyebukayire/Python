fe5909434a1e129774972378a77c1eb1ba05e180
Cyebukayire <peacecyebukayire@gmail.com>
2021-08-05 07:53:50 +0200

    debug files

--- 5.Classes/person.py
+++ 5.Classes/person.py
@@ -1,7 +1,8 @@
 
-class Person(): #If a class name is made of two or more names we write it like: Class MammalAnimals()
+class Person: #If a class name is made of two or more names we write it like: Class MammalAnimals()
     def __init__(self, name):
         self.name = name
+
     def talk(self):
         print("Hi! I am "+ self.name)
         return None
--- 7.Random/randomFile.py
+++ 7.Random/randomFile.py
@@ -1,14 +1,15 @@
 import random
 
 
-class Dice():
+def roll():
+    first_choice = random.randint(1, 6)
+    second_choice = random.randint(1, 6)
+    return first_choice, second_choice
 
-    def roll(self):
-        first_choice = random.randint(1, 6)
-        second_choice = random.randint(1, 6)
-        return first_choice, second_choice 
 
+class Dice:
+    pass
 
 
 dice1 = Dice()
-print(dice1.roll())
+print(roll())
