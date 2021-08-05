23f84ebc55649091a0689487bf26fb4f064a932f
Cyebukayire <peacecyebukayire@gmail.com>
2021-08-05 08:01:12 +0200

    debug files

--- 5.Classes/person.py
+++ 5.Classes/person.py
@@ -1,5 +1,5 @@
 
-class Person: #If a class name is made of two or more names we write it like: Class MammalAnimals()
+class Person:  # If a class name is made of two or more names we write it like: Class MammalAnimals()
     def __init__(self, name):
         self.name = name
 
--- 5.Classes/point.py
+++ 5.Classes/point.py
@@ -1,19 +1,18 @@
-class  Point:
+class Point:
     # We need constructors to initialize more objects
     def __init__(self, x, y, z):
         self.x = x
         self.y = y
         self.z = z
-                
 
     def move(self):
         print("move")
-    
 
     def draw(self):
         print("draw")
         return 23
 
-point1 = Point(23,34, 44)
+
+point1 = Point(23, 34, 44)
 point1.draw()
-print(point1.z)
+print(point1.z, point1.draw())
