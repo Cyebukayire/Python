01e641e6889d6f0dde75d25daecc38c58c05a107
Cyebukayire <peacecyebukayire@gmail.com>
2021-08-05 15:12:42 +0200

    accessing data from excel

--- 9. excel/transaction.py
+++ 9. excel/transaction.py
@@ -2,6 +2,14 @@ import openpyxl as xl
 workbook = xl.load_workbook("transactions.xlsx")
 sheet1 = workbook['Sheet1']
 cell1 = sheet1['a1']
-cell1 = sheet1.cell(1, 1)
+cell1 = sheet1.cell(1, 23)
 
 print(cell1.value)
+
+#  Print all the values
+row = 1
+col = 1
+while sheet1.cell(row, col):
+    print(sheet1.cell(row, col))
+    row += 1
+    col += 1
