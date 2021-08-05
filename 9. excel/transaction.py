90efd06d62130f6c630921b5e26e9ba45912c041
Cyebukayire <peacecyebukayire@gmail.com>
2021-08-05 12:36:23 +0200

    Cells

--- 9. excel/transaction.py
+++ 9. excel/transaction.py
@@ -1,3 +1,8 @@
 import openpyxl as xl
-xl.load_workbook("transactions.xlsx")
+workbook = xl.load_workbook("transactions.xlsx")
+sheet1 = workbook['Sheet1']
+cell1 = sheet1['a1']
+sheet1.cell(1, 1)
 
+print(sheet1.cell(1, 1))
+print(cell1)
