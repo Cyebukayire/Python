0b250fc4044e8694cb3084e31cd504c93557a62c
Cyebukayire <peacecyebukayire@gmail.com>
2021-08-06 10:34:33 +0200

    function for automatic excel calculations

--- 9. excel/transaction.py
+++ 9. excel/transaction.py
@@ -1,33 +1,28 @@
 import openpyxl as xl
 from openpyxl.chart import BarChart, Reference
 
-workbook = xl.load_workbook("transactions.xlsx")
-sheet1 = workbook['Sheet1']
 
-# cell1 = sheet1['a1']
-# cell1 = sheet1.cell(1, 23)
-# print(cell1.value)
+def process(filename):
+    workbook = xl.load_workbook(filename)
+    sheet1 = workbook['Sheet1']
 
-# print(sheet1.max_column)
-# print(sheet1.max-row)
+    for row in range(2, sheet1.max_row + 1):
+        cell = sheet1.cell(row, 3)
+        correct_price = cell.value * 0.9
+        correct_price_cell = sheet1.cell(row, 4)
+        correct_price_cell.value = correct_price
+    sheet1.cell(1, 4).value = "Correct Price"
 
-#  Print all the values
-for row in range(2, sheet1.max_row + 1):
-    cell = sheet1.cell(row, 3)
-    correct_price = cell.value * 0.9
-    correct_price_cell = sheet1.cell(row, 4)
-    correct_price_cell.value = correct_price
-sheet1.cell(1, 4).value = "Correct Price"
+    values = Reference(sheet1,
+                       min_row=2,
+                       max_row=sheet1.max_row,
+                       min_col=4,
+                       max_col=4)
 
-values = Reference(sheet1,
-          min_row=2,
-          max_row=sheet1.max_row,
-          min_col=4,
-          max_col=4)
+    chart = BarChart()
+    chart.add_data(values)
+    sheet1.add_chart(chart, 'e2')
 
-chart = BarChart()
-chart.add_data(values)
-sheet1.add_chart(chart, 'e2')
+    workbook.save(filename)
 
-workbook.save("transactions2.xlsx")
 
